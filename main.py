cd91d14493f1175ec3a61c2b5ce8ff1d51fac351
WamashuduSigogo-LFI <sigogowamashudu@gmail.com>
2024-07-29 11:25:41 +0200

    Created a while loop to verify user input

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def roll():
     roll = random.randint(min_val,max_val)
 
     return roll
+
+while True:
+    players = input("Enter total number of players(2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Invalid number of players, please select numbers in the range (2-4)")
