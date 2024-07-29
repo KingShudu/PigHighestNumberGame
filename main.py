66c4e5125465d5d8a56a8b20a4201358cafef9ab
WamashuduSigogo-LFI <sigogowamashudu@gmail.com>
2024-07-29 11:51:24 +0200

    Created main game logic

--- main.py
+++ main.py
@@ -15,3 +15,30 @@ while True:
             break
         else:
             print("Invalid number of players, please select numbers in the range (2-4)")
+    else:
+        print("Error encountered")
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+while max(player_scores) < max_score:
+
+    for playerIndex in range(players):
+        print("\nPlayer number",playerIndex +1 ,"'s turn has started\n")
+        current_score=0
+        while True:
+            should_roll = input("Would you like to roll? (y)")
+            if should_roll.lower() != "y":
+                break
+            value = roll()
+            if value == 1:
+                print("Your turn is over you rolled a 1")
+                current_score = 0
+                break
+
+            else:
+                current_score += value
+                print("You rolled a ", value)
+
+            print("Your current score is: ", current_score)
+        player_scores[playerIndex] += current_score
+        print("Your total score is: ", player_scores[playerIndex])
